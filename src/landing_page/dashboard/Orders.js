55c2ae0462e7b7d401d434d4e2390c057ccf7a13
shreyanshshardul <nikhilsinghk143@gmail.com>
2025-10-18 08:43:34 +0530

    Integrated

---
+++ src/landing_page/dashboard/Orders.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "./src/config";
+
+const Orders = () => {
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/allOrders`) // make sure this route exists in backend
+      .then((res) => setItem(res.data))
+      .catch((err) => console.error("Error fetching orders:", err));
+  },[]);
+
+  return (
+    <>
+      <h3 className="title">All Orders ({item.length})</h3>
+      <div className="order-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Qty.</th>
+              <th>Price</th>
+              <th>Mode</th>
+            </tr>
+          </thead>
+          <tbody>
+            {item.map((stock, index) => (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{stock.mode}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Orders;
