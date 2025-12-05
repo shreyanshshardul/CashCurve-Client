110262da03e7f29e68a35a8d2858d74ea34a140a
shreyanshshardul <nikhilsinghk143@gmail.com>
2025-12-05 14:38:36 +0530

    Updated frontend after switching repo

--- src/landing_page/OpenAccount.js
+++ src/landing_page/OpenAccount.js
@@ -1,13 +1,26 @@
 import React from 'react'
- 
-export default function OpenAccount(){
-    return (
-        <div className="container-fluid p-5">
-            <div className="row text-center" >
-            <h1 className="mt-5 mb-3">Open a CashCurve account</h1>
-            <p className="text-muted mb-3">Modern platfroms and apps , &#8377;0 investment and flat &#8377;20 intraday and F&O trades</p>
-            <button className="p-2 btn btn-primary mb-5 mt-3" style={{margin: "0 auto" ,width:"20%"}}>Signup Now</button>
-            </div>
-        </div>
-    )
+import { useNavigate } from "react-router-dom";
+
+export default function OpenAccount() {
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="container-fluid p-5">
+      <div className="row text-center">
+        <h1 className="mt-5 mb-3">Open a CashCurve account</h1>
+        <p className="text-muted mb-3">
+          Modern platfroms and apps , ₹0 investment and flat ₹20 intraday and F&O trades
+        </p>
+
+        <button
+          className="p-2 btn btn-primary mb-5 mt-3"
+          style={{ margin: "0 auto", width: "20%" }}
+          onClick={() => navigate("/signup")}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  )
 }
